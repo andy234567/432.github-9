1fbbc282b4cbae5da76ce321b2cd6da75949ca1a
andy234567 <68265634+andy234567@users.noreply.github.com>
2020-07-16 00:37:54 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$('.slider2').slick({
+  dots: true
+});
+
+$('.slider').slick({
+  centerMode: true,
+    centerPadding: '100px',
+    dots:true,
+    focusOnSelect:true,
+   autoplay: true,
+   autoplaySpeed: 3000,
+   dots: true,
+   pauseOnHover:false,
+   pauseOnFocus:false,
+   arrows:false,
+});
+
+$('.slider3').slick({
+  dots: true,
+ 　　arrows: true,
+ 　　slidesToShow:3,
+ 　});
+ $('.slider5').slick({
+   dots: true,
+  　　arrows: true,
+  　　slidesToShow:3,
+  　});
